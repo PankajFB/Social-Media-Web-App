dc62a9faebcc7bab4998f6d5af2d33e082834165
Pankaj kumar <pk497243@gmail.com>
2023-05-05 20:20:31 +0530

    userlist done

---
+++ Project-X-in-next-main/components/Dashboard/ConnectedUsers.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+function ConnectedUsers(props) {
+console.log(props.connectedUsers)
+
+  return (
+    <div>
+      <div className="card mb-4">
+        <div className="card-header pb-0">
+          <h6>Other Users</h6>
+        </div>
+        <div className="card-body px-0 pt-0 pb-2">
+          <div className="table-responsive p-0">
+            <table className="table align-items-center mb-0">
+              <thead>
+                <tr>
+                  <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                    Author
+                  </th>
+                  <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                    Function
+                  </th>
+                  <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                    Status
+                  </th>
+                  <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                    Employed
+                  </th>
+                  <th className="text-secondary opacity-7" />
+                </tr>
+              </thead>
+              <tbody>
+                {/* we will render the conneteduser list here */}
+                {/* <h1>This is my name : {props.connectedUsers}</h1> */}
+
+                {props.connectedUsers.map((user) => (
+                  
+                    <tr>
+                      <td>
+                        <div className="d-flex px-2 py-1">
+                          <div>
+                            <img
+                              src={user.photoURL}
+                              alt="DP"
+                              className="avatar avatar-sm me-3"
+                            />
+                          </div>
+                          <div className="d-flex flex-column justify-content-center">
+                            <h6 className="mb-0 text-sm">{user.displayName}</h6>
+                            <p className="text-xs text-secondary mb-0">
+                              {user.email}
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p className="text-xs font-weight-bold mb-0">{user.socketId}</p>
+                        <p className="text-xs text-secondary mb-0">
+                          Organization
+                        </p>
+                      </td>
+                      <td className="align-middle text-center text-sm">
+                        <span className="badge badge-sm bg-gradient-success">
+                          Online
+                        </span>
+                      </td>
+                      <td className="align-middle text-center">
+                        <span className="text-secondary text-xs font-weight-bold">
+                          23/04/18
+                        </span>
+                      </td>
+                      <td className="align-middle">
+                        <a
+                          href="/"
+                          className="text-secondary font-weight-bold text-xs"
+                          data-toggle="tooltip"
+                          data-original-title="Edit user"
+                        >
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
+                  
+                ))}
+
+
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ConnectedUsers;
--- Project-X-in-next-main/components/Dashboard/Dashboard.js
+++ Project-X-in-next-main/components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { useFirebase } from "../../Context/FirebaseContext";
 import { signOut } from "firebase/auth";
 // import { SocketContext } from "../../Context/SocketContext";
 import { io } from "socket.io-client";
 import { useSocket } from "@/Context/SocketContext";
+import ConnectedUsers from "./ConnectedUsers";
 
 function Dashboard() {
   const firebase = useFirebase();
@@ -15,11 +16,15 @@ function Dashboard() {
   const socket = useSocket();
   console.log(socket);
 
-  const connectedUsers = [];
-  // const connectedUsers = [
+  const [connectedUsers, setConnectedUsers] = useState([]);
+
   //   { socketId: "123", displayName: "Pankaj" },
   //   { socketId: "123", displayName: "Saroj" },
   // ];
+  // const connectedUsers = [];
+
+
+
 
   useEffect(() => {
     // socket.on("message", (data) => {
@@ -31,7 +36,9 @@ function Dashboard() {
       const { displayName, email, photoURL, socketId } = data;
       // setConnectedUsers(...connetedusers, { socketId,  displayName, email, photoURL})
       if (data.displayName) {
-        connectedUsers.push({ socketId, displayName, email, photoURL });
+        const newUser = { socketId, displayName, email, photoURL };
+        // connectedUsers.push({ socketId, displayName, email, photoURL });
+        setConnectedUsers([...connectedUsers, newUser]);
         console.log(connectedUsers);
       }
     });
@@ -41,6 +48,7 @@ function Dashboard() {
     // });
 
     return () => {
+      socket.off("refresh_user_list")
       socket.off("message");
     };
   }, []);
@@ -653,90 +661,15 @@ function Dashboard() {
                 </div>
               </div>
             </div>
-            <div className="card mb-4">
-              <div className="card-header pb-0">
-                <h6>Other Users</h6>
-              </div>
-              <div className="card-body px-0 pt-0 pb-2">
-                <div className="table-responsive p-0">
-                  <table className="table align-items-center mb-0">
-                    <thead>
-                      <tr>
-                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                          Author
-                        </th>
-                        <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                          Function
-                        </th>
-                        <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                          Status
-                        </th>
-                        <th className="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                          Employed
-                        </th>
-                        <th className="text-secondary opacity-7" />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {/* we will render the conneteduser list here */}
+            {/* our connected user components will be here */}
 
-                      {connectedUsers.map((user) => (
-                        <div>
-                          <tr>
-                            <td>
-                              <div className="d-flex px-2 py-1">
-                                <div>
-                                  <img
-                                    src="https://demos.creative-tim.com/soft-ui-dashboard/assets/img/team-2.jpg"
-                                    className="avatar avatar-sm me-3"
-                                  />
-                                </div>
-                                <div className="d-flex flex-column justify-content-center">
-                                  <h6 className="mb-0 text-sm">
-                                    {user?.displayName}
-                                  </h6>
-                                  <p className="text-xs text-secondary mb-0">
-                                    john@creative-tim.com
-                                  </p>
-                                </div>
-                              </div>
-                            </td>
-                            <td>
-                              <p className="text-xs font-weight-bold mb-0">
-                                Manager
-                              </p>
-                              <p className="text-xs text-secondary mb-0">
-                                Organization
-                              </p>
-                            </td>
-                            <td className="align-middle text-center text-sm">
-                              <span className="badge badge-sm bg-gradient-success">
-                                Online
-                              </span>
-                            </td>
-                            <td className="align-middle text-center">
-                              <span className="text-secondary text-xs font-weight-bold">
-                                23/04/18
-                              </span>
-                            </td>
-                            <td className="align-middle">
-                              <a
-                                href="/"
-                                className="text-secondary font-weight-bold text-xs"
-                                data-toggle="tooltip"
-                                data-original-title="Edit user"
-                              >
-                                Edit
-                              </a>
-                            </td>
-                          </tr>
-                        </div>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+            { connectedUsers? (
+        <ConnectedUsers connectedUsers={connectedUsers}></ConnectedUsers>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+           
           </div>
           <footer className="footer pt-3 pb-4">
             <div className="container-fluid">
